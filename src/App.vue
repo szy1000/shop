f2eab2a7b7949f8538a2d4f6138ae3fd46dc79dd
szy1000 <1538078053@qq.com>
2017-08-20 11:16:31 +0800

    子组件调用父组件的方法

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <child :num="num" @forChildAdd="parnentAdd" @forCHildDecrease="parentDecrase"></child>
+    {{num}}
+  </div>
+</template>
+
+<script>
+import child from './components/child.vue';
+export default {
+  name: 'app',
+  data () {
+    return {
+      num: 15
+    };
+  },
+  components: {
+    child
+  },
+  methods: {
+    parnentAdd () {
+      this.num++;
+    },
+    parentDecrase () {
+      this.num--;
+    }
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/child.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="child">
+  	<button @click="add">+</button>
+  	{{num}}
+  	<button @click="decrease">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+	props: {
+		num: {
+			type: Number,
+			default: 0
+		}
+	},
+	// data () {
+	// 	return {
+	// 		num: 10
+	// 	};
+	// },
+	methods: {
+		add () {
+			// this.num++;
+			this.$emit('forChildAdd');
+		},
+		decrease () {
+			// this.num--;
+			this.$emit('forCHildDecrease');
+		}
+	}
+};
+</script>
+
+<style scoped lang="scss">
+	.child{
+
+	}
+</style>
